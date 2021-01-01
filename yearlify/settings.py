7d1b817af177d8e608974597c8b5ffdb2185ca2f
Enrique Tasa <tasa@enriquetasa.com>
2021-01-01 18:41:28 +0100

    Ensure heroku URL is allowed

--- yearlify/settings.py
+++ yearlify/settings.py
@@ -3,7 +3,10 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['yearlify.com']
+ALLOWED_HOSTS = [
+    'yearlify.com',
+    'yearlify.herokuapp.com'
+    ]
 
 # SECURITY
 SECRET_KEY = config('SECRET_KEY')
