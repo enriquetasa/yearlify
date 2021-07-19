433d7734406d7e3a9c4a23f3d97989a58108c69b
Enrique Tasa <tasa@enriquetasa.com>
2021-07-19 09:32:06 +0200

    Adds email config

--- yearlify/settings.py
+++ yearlify/settings.py
@@ -105,3 +105,12 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # CUSTOMISATION
 AUTH_USER_MODEL = 'accounts.User'
+
+# EMAIL
+#DataFlair
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yearlify@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
