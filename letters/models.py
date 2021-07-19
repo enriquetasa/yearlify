ae7f8cd21c9ae38c956d9f97d8efdacac5eb502e
Enrique Tasa <tasa@enriquetasa.com>
2021-07-19 09:32:24 +0200

    Adds processing to Letter model

--- letters/models.py
+++ letters/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 from accounts.models import User
+from yearlify.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
 
 from django_cryptography.fields import encrypt
+import datetime
 
 class Letter(models.Model):
     user = models.ForeignKey(User, related_name='user_letter', on_delete=models.PROTECT)
@@ -14,4 +17,13 @@ class Letter(models.Model):
     def __str__(self):
         return "Letter by {0} created on {1}".format(self.user.email, self.created_at.strftime('%D'))
 
-    # TODO - process method
+    def process(self):
+        subject = "Yearlify letter for {0}".format(self.user.name)
+        message = self.content 
+        send_mail(subject, message, EMAIL_HOST_USER, [self.user.email], fail_silently = False)
+
+    @classmethod
+    def process_all(cls):
+        for letter in cls.objects.all():
+            if letter.send_on == datetime.today().date():
+                letter.process()
